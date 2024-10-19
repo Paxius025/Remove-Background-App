2f96cea7e3bef9cb0ee67aae61303e894e255944
pantong <pantongbk@gmail.com>
2024-10-19 15:34:30 +0700

    fix: handle RGBA mode when saving images as JPEG/PNG

--- utils.py
+++ utils.py
@@ -3,20 +3,35 @@ from PIL import Image
 import os
 import time
 
-def remove_background(input_path, output_path):
+def remove_background(input_path, export_path):
     try:
         # Open the image
         input_image = Image.open(input_path)
-        # Process the image using rembg
+        # Get the original size of the image
+        original_size = input_image.size
+        
+        # Process the image using rembg and U-2-Net model
         output_image = remove(input_image)
+
+        # Resize the output image to match the original size
+        output_image = output_image.resize(original_size, Image.Resampling.LANCZOS)
+
         # Create a unique file name based on the current timestamp and original file name
         base_name = os.path.basename(input_path)
         name, ext = os.path.splitext(base_name)
         timestamp = int(time.time())
-        new_name = f"{name}_{timestamp}{ext}"
-        output_file = os.path.join(output_path, new_name)
-        # Save the processed image
-        output_image.save(output_file)
+
+        # Save as PNG if transparency (RGBA) exists, otherwise save as JPEG
+        if output_image.mode == "RGBA":
+            new_name = f"{name}_{timestamp}.png"
+            output_file = os.path.join(export_path, new_name)
+            output_image.save(output_file, format="PNG", quality=95)
+        else:
+            new_name = f"{name}_{timestamp}.jpg"
+            output_file = os.path.join(export_path, new_name)
+            output_image = output_image.convert("RGB")  # Convert to RGB for JPEG
+            output_image.save(output_file, format="JPEG", quality=95)
+
         return output_file
     except Exception as e:
         print(f"Error removing background: {e}")
